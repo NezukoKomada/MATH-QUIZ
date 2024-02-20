ca14f29484875c69574d360da423dc84cf142cff
NezukoKomada <159033240+NezukoKomada@users.noreply.github.com>
2024-02-20 19:24:42 +0400

    Delete quizlogin.js

--- quizlogin.js
+++
@@ -1,7 +0,0 @@
-function addUser() {
-    player1name = document.getElementById("player1name").value;
-    player2name = document.getElementById("player2name").value;
-    localStorage.setItem("player1 name", player1name);
-    localStorage.setItem("player2 name", player2name);
-    window.location = "quiz.html";
-}
